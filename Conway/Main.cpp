7cd6818a65439be24a4c339dfaa1283e4e6600b5
goodbyeLennon <mjrskiles@gmail.com>
2016-02-14 01:21:52 -0600

    Added basic class for game

--- Conway/Main.cpp
+++ Conway/Main.cpp
@@ -4,6 +4,7 @@ The goal of this program is play Conway's Game of Life on an arbitrarily sized f
 given a starting condition from a text file.  The game will be displayed in ASCII text.  In 
 this version, if a live cell goes off the board, it wraps around the other edge.
 
+----------------------------------------------------------------
 desired future features:
 -Have an unlimited number of steps and the game will end when the user hits ESC.
 
@@ -14,6 +15,10 @@ desired future features:
 
 -Have an interactive menu for changing settings.
 
+-add classes
+
+-dynamic arrays for the grids after determining the grid size in the file loaded at runtime.
+
 
 --------------------------rules---------------------------------
 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
@@ -22,7 +27,6 @@ desired future features:
 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 */
 
-//comment to test git
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -37,28 +41,30 @@ const int FRAMEDELAY = 150; //time between frames in ms
 const char LIVE = 'O';
 const char DEAD = ' ';
 
-void fillGrid(std::ifstream& in_s, int grid[][XSIZE], const int y, const int x); //gets the starting grid from a file and fills the base game grid
-void updateGrid(int currentGrid[][XSIZE], int nextGrid[][XSIZE], const int y, const int x); //calculate the next grid iteration
-void drawGrid(const int grid[][XSIZE], const int y, const int x); //draw the grid on screen.
-bool cellChecker(const int grid[][XSIZE], const int ysize, const int xsize, int yLoc, int xLoc); //determines whether an individual cell is alive or dead.
+class Game {
+public:
+			Game( std::ifstream& startGrid, int currentGrid[][XSIZE], const int YSIZE, const int XSIZE );
+	void	fillGrid( std::ifstream& in_s, int grid[][XSIZE], const int y, const int x ); //gets the starting grid from a file and fills the base game grid
+	void	updateGrid( int currentGrid[][XSIZE], int nextGrid[][XSIZE], const int y, const int x ); //calculate the next grid iteration
+	void	drawGrid( const int grid[][XSIZE], const int y, const int x ); //draw the grid on screen.
 
-int main()
-{
+private:
+	bool	cellChecker(const int grid[][XSIZE], const int ysize, const int xsize, int yLoc, int xLoc); //determines whether an individual cell is alive or dead.
+
+};
+
+int main() {
 	//variables
 	std::ifstream startGrid; //the stream to open the starting grid file
 	int currentGrid[YSIZE][XSIZE] = {}; //the current grid, which is displayed and used to calculate the next iteration
 	int nextGrid[YSIZE][XSIZE] = {}; //the grid that is filled with the next iteration of the game
 	int stepNumber = 0; // the current iteration of the game
 
-	std::cout << "\t\t===Conway's Game of Life::";
-	std::cout << XSIZE << " x " << YSIZE << " edition===\n\n";
+	Game gol( startGrid, currentGrid, YSIZE, XSIZE ); //main game object
 
-	fillGrid(startGrid, currentGrid, YSIZE, XSIZE);
-
-	while(stepNumber < STEPS)
-	{
-		drawGrid(currentGrid, YSIZE, XSIZE);
-		updateGrid(currentGrid, nextGrid, YSIZE, XSIZE);
+	while( stepNumber < STEPS ) {
+		gol.drawGrid(currentGrid, YSIZE, XSIZE);
+		gol.updateGrid(currentGrid, nextGrid, YSIZE, XSIZE);
 		stepNumber++;
 		std::cout << "\n\n" << stepNumber;
 	}
@@ -67,8 +73,16 @@ int main()
 
 }
 
-void fillGrid(std::ifstream& in_s, int grid[][XSIZE], int y, int x)
-{
+Game::Game( std::ifstream& startGrid, int currentGrid[][XSIZE], const int YSIZE, const int XSIZE ) {
+	
+	std::cout << "\t\t===Conway's Game of Life::";
+	std::cout << XSIZE << " x " << YSIZE << " edition===\n\n";
+
+	fillGrid(startGrid, currentGrid, YSIZE, XSIZE);
+
+}
+
+void Game::fillGrid(std::ifstream& in_s, int grid[][XSIZE], int y, int x) {
 	using namespace std;
 
 	string fileName;
@@ -79,22 +93,20 @@ void fillGrid(std::ifstream& in_s, int grid[][XSIZE], int y, int x)
 	cin >> fileName;
 
 	in_s.open(fileName); 	 	//  Connect to the input file and test 
-    if( in_s.fail() )
-    { 
+
+    if( in_s.fail() ) { 
        cout << "Input file opening failed. \n"; 
        exit(1); 				 // if we couldn't open the file to read from we exit 
     }
 
-	for(int i = 0; i < y; i++)
-	{
-		for(int j = 0 ; j < x; j++)
-		{
-			if( (in_s >> next) ) 
-			{
-				
+	for(int i = 0; i < y; i++) {
+
+		for(int j = 0 ; j < x; j++) {
+
+			if( (in_s >> next) ) {				
 				nextInt = next - '0'; //convert from char to int
-				switch(nextInt)
-				{
+
+				switch(nextInt) {
 					case 1:
 						grid[i][j] = 1;
 						break;
@@ -107,8 +119,7 @@ void fillGrid(std::ifstream& in_s, int grid[][XSIZE], int y, int x)
 			}
 
 			//if there are not enough numbers, fill the rest of the table with zeros.
-			else
-			{
+			else {
 				grid[i][j] = 0;
 				cout << "\nThe input file is not formatted properly. Grid location ";
 				cout << "(" << i << ", " << j << ") was automatically filled with a 0.";
@@ -121,8 +132,7 @@ void fillGrid(std::ifstream& in_s, int grid[][XSIZE], int y, int x)
 
 }
 
-void drawGrid(const int grid[][XSIZE], const int y, const int x)
-{
+void Game::drawGrid(const int grid[][XSIZE], const int y, const int x) {
 	using namespace std;
 	int next;
 
@@ -130,19 +140,17 @@ void drawGrid(const int grid[][XSIZE], const int y, const int x)
 	system("CLS");		//clear the screen before drawing the next frame.
 	cout << "\n\n";
 
-	for(int i = 0; i < y; i++)
-	{
+	for(int i = 0; i < y; i++) {
 		cout << endl;
 
-		for(int j = 0; j < x; j++)
-		{
+		for(int j = 0; j < x; j++) {
 			next = grid[i][j];
-			if(next == 1)
-			{
+
+			if(next == 1) {
 				cout << LIVE;
 			}
-			else
-			{
+
+			else {
 				cout << DEAD;
 			}
 		}
@@ -150,59 +158,51 @@ void drawGrid(const int grid[][XSIZE], const int y, const int x)
 
 }
 
-void updateGrid(int currentGrid[][XSIZE], int nextGrid[][XSIZE], const int y, const int x)
-{
+void Game::updateGrid(int currentGrid[][XSIZE], int nextGrid[][XSIZE], const int y, const int x) {
 	bool cellState;
 
 	//create the next grid
-	for(int i = 0; i < y; i++)
-	{
-		for(int j = 0; j < x; j++)
-		{
+	for(int i = 0; i < y; i++) {
+
+		for(int j = 0; j < x; j++) {
 			cellState = cellChecker(currentGrid, YSIZE, XSIZE, i, j);
-			if(cellState == 1)
-			{
+
+			if(cellState == 1) {
 				nextGrid[i][j] = 1;
 			}
-			else
-			{
+
+			else {
 				nextGrid[i][j] = 0;
 			}
 		}
 	}
 
 	//copy the new grid to the current grid in preparation for the next iteration.
-	for(int i = 0; i < y; i++)
-	{
-		for(int j = 0; j < x; j++)
-		{
+	for(int i = 0; i < y; i++) {
+
+		for(int j = 0; j < x; j++) {
 			currentGrid[i][j] = nextGrid[i][j];
 		}
 	}
 }
 
-bool cellChecker(const int grid[][XSIZE], const int ysize, const int xsize, int yLoc, int xLoc)
-{
+bool Game::cellChecker(const int grid[][XSIZE], const int ysize, const int xsize, int yLoc, int xLoc) {
 	
 	int yCheck; //the current y position being checked.
 	int xCheck;
 	int liveNeighbors = 0; //the running total number of live neighbor cells.
 
-	for(int i = 0; i < 3; i++)
-	{
+	for(int i = 0; i < 3; i++) {
 		yCheck = ((yLoc + (i - 1) + ysize) % ysize); //calculate the current row to check.  Wrap around the grid if there is not a grid above/below the current one.
-		for(int j = 0; j < 3; j++)
-		{
+		for(int j = 0; j < 3; j++) {
+
 			xCheck = ((xLoc + (j - 1) + xsize) % xsize);
-			//std::cout << "\nLoc (" << yCheck << "," << xCheck << " is " << grid[yCheck][xCheck]; //for testing.
-			if( grid[yCheck][xCheck] == 1 ) //if the cell is alive, add one to the running counter of live neighbors.
-			{
-				if(yCheck == yLoc && xCheck == xLoc)
-				{
+			///*TEST*/ std::cout << "\nLoc (" << yCheck << "," << xCheck << " is " << grid[yCheck][xCheck]; //for testing.
+			if( grid[yCheck][xCheck] == 1 ) { //if the cell is alive, add one to the running counter of live neighbors. 
+				if(yCheck == yLoc && xCheck == xLoc) {
 					//do nothing
 				}
-				else
-				{
+				else {
 					liveNeighbors++;
 				}
 			}
@@ -213,29 +213,25 @@ bool cellChecker(const int grid[][XSIZE], const int ysize, const int xsize, int 
 	//std::cout << "\nliveNeighbors = " << liveNeighbors << "\n";
 
 	//calc the return conditions if the cell is already alive.
-	if( grid[yLoc][xLoc] == 1 )
-	{
-		if( liveNeighbors < 2 || liveNeighbors > 3 )
-		{
+	if( grid[yLoc][xLoc] == 1 ) {
+
+		if( liveNeighbors < 2 || liveNeighbors > 3 ) {
 			return 0;
 		}
 
-		if( liveNeighbors == 2 || liveNeighbors == 3 )
-		{
+		if( liveNeighbors == 2 || liveNeighbors == 3 ) {
 			return 1;
 		}
 	}
 
-	if( grid[yLoc][xLoc] == 0 )
-	{
-		if( liveNeighbors == 3 )
-		{
+	if( grid[yLoc][xLoc] == 0 ) {
+
+		if( liveNeighbors == 3 ) {
 			return 1;
 		}
 	}
 
-	if( grid[yLoc][xLoc] != 0 && grid[yLoc][xLoc] != 1 )
-	{
+	if( grid[yLoc][xLoc] != 0 && grid[yLoc][xLoc] != 1 ) {
 		std::cout << "\nSomething went wrong...\n";
 		exit(1);
 	}
